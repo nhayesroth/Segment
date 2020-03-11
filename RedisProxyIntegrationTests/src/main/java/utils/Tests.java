65e1d225886210730f1f7fc0d550279852a52d82
nhayes-roth <nathan.hayesroth@gmail.com>
2020-03-10 20:11:24 -0700

    The IT actually runs! But now I want it to run as a test as opposed to a main class.

--- RedisProxyIntegrationTests/src/main/java/utils/Tests.java
+++ RedisProxyIntegrationTests/src/main/java/utils/Tests.java
@@ -8,6 +8,7 @@ public class Tests {
   public static void main(String[] args) {
     System.out.println("Running tests!");
     JUnitCore engine = new JUnitCore();
+    engine.run(AllTests.class);
     engine.addListener(new TextListener(System.out));
   }
 }
